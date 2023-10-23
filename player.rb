9df71cd5b533e3fa1f959567a827aa5f0c4d6250
Afreeda <64160666+afreeda-m@users.noreply.github.com>
2023-10-23 15:15:53 -0400

    remove is_alive method

--- player.rb
+++ player.rb
@@ -1,21 +1,21 @@
+# define a Player class to set player information
 class Player
   attr_reader :name, :lives, :score
 
+  # set initial data
   def initialize(name)
     @name = name
     @lives = 3
     @score = 0
   end
 
+  # set lives
   def decrease_lives
     @lives -= 1
   end
 
+  # set score
   def update_score
     @score += 1
-  end
-
-  def is_alive?
-    @lives > 0
   end
 end
