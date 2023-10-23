26b90d0af3617e159003df6097fd73de9b542453
Afreeda <64160666+afreeda-m@users.noreply.github.com>
2023-10-23 15:15:42 -0400

    add code to start game

--- main.rb
+++ main.rb
@@ -1,3 +1,9 @@
 require_relative 'player'
 require_relative 'question'
 require_relative 'game'
+
+# create a new instance of the game class (ie, create a new game)
+game = Game.new
+
+# call the start method of the game class
+game.start
